47736d7a93f18f6047c795ddb4b57e710aee406b
henrylemmon <henrylemmon@icloud.com>
2020-06-16 16:56:14 -0700

    Components Explained

---
+++ resources/views/components/section.blade.php
@@ -0,0 +1,7 @@
+<section class="bg-gray-300 p-4 mb-4">
+    <div class="container">
+        <p>
+            {{ $slot }}
+        </p>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-layout>
+    <x-section>
+        Welcome
+    </x-section>
 
-        <title>Laravel</title>
+    <x-section>
+        Welcome Again
+    </x-section>
 
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-
-    </head>
-    <body>
-
-    </body>
-</html>
+    <x-section>
+        Welcome A Third Time
+    </x-section>
+</x-layout>
