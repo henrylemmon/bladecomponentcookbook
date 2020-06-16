7c07b55f4dbe033c0d61894e16a8d62a8bd3bdc9
henrylemmon <henrylemmon@icloud.com>
2020-06-16 16:42:10 -0700

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+    </body>
+</html>
