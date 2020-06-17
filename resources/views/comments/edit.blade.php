38584f82dec61237caabafa5869099844909d178
henrylemmon <henrylemmon@icloud.com>
2020-06-16 21:35:31 -0700

    Form Component

---
+++ resources/views/comments/edit.blade.php
@@ -0,0 +1,35 @@
+<x-layout>
+    <x-section>
+        <x-form
+            method="PATCH"
+            action="/comments/{{ $comment->id }}"
+            class="bg-blue-200"
+        >
+            <div class="mb-6">
+                <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                    Body
+                </label>
+
+                <textarea
+                    name="body"
+                    id="body"
+                    required
+                    class="border border-gray-400 p-2 w-full"
+                >{{ $comment->body }}</textarea>
+
+                @error('body')
+                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <button
+                    type="submit"
+                    class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
+                >
+                    Submit
+                </button>
+            </div>
+        </x-form>
+    </x-section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Comment;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/comments/{comment}/edit', function (Comment $comment) {
+   return view('comments.edit', ['comment' => $comment]);
+});
+
+Route::patch('/comments/{comment}', function (Comment $comment) {
+    $comment->update(
+        request()->validate(['body' => 'required|string'])
+    );
+
+    return redirect("/comments/{$comment->id}/edit");
 });
